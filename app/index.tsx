aaacbee6e45832e39659891efc30a7a388eb6e60
b-cheek <cheekbw@gmail.com>
2025-02-16 23:12:34 -0500

    Remove proof of concept for more or less clean slate

--- app/index.tsx
+++ app/index.tsx
@@ -1,41 +1,19 @@
-import { WebView } from 'react-native-webview';
-import Constants from 'expo-constants';
-import { StyleSheet } from 'react-native';
+import { Text, View,  StyleSheet } from 'react-native';
 
-export default function App() {
+export default function Index() {
   return (
-    <WebView
-      style={styles.container}
-      // source={{ uri: 'https://nbrosowsky.github.io/tonejs-instruments/demo.html' }}
-      originWhitelist={['*']}
-      source={{ html:
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Tone.js Example</title>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.40/Tone.js"></script>
-</head>
-<body>
-  <button onclick="playSound()">Play Sound</button>
-  <script>
-    function playSound() {
-      const synth = new Tone.Synth().toDestination();
-      synth.detune.value = 200;
-      synth.triggerAttackRelease("C4", "8n");
-    }
-  </script>
-</body>
-</html>`
-      }}
-    />
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Constants.statusBarHeight,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
   },
 });
