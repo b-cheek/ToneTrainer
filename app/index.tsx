4b676830265463c640a3ad458048f57c3bcd53c7
b-cheek <cheekbw@gmail.com>
2025-02-22 23:27:49 -0500

    Basic exercise/home page layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,37 @@
-import { Text, View,  StyleSheet, Button } from 'react-native';
+import { Text, View, FlatList, Button, StyleSheet } from 'react-native';
 
 export default function Index() {
   return (
+    <View style={styles.container}>
       <View style={styles.container}>
-        <Text style={styles.text}>Exercises</Text>
+        <Text style={styles.text0}>Exercises</Text>
       </View>
+      <View style={styles.exerciseContainer}>
+        <FlatList
+          data={[{key: 'A'}, {key: 'B'}, {key: 'C'}]}
+          renderItem={({item}) => <Button title={item.key} onPress={() => {}} />}
+          keyExtractor={item => item.key}
+        >
+        </FlatList>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
+  text0: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  exerciseContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
 });
