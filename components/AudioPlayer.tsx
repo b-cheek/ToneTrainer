be1519434a6489cf9ef8623685967aa0963907a4
b-cheek <cheekbw@gmail.com>
2025-03-10 14:12:52 -0400

    More or less figure out webview message passing with tone js unless offline audio context seems like it would be helpful later. Also getting started in direction of exercise customizability

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,7 +1,26 @@
 import React, { forwardRef } from 'react';
 import { WebView } from 'react-native-webview';
 
-const AudioPlayer = forwardRef<WebView, { soundScript: string} >(({ soundScript }, ref) => {
+const AudioPlayer = forwardRef<WebView, { soundScript: string} >((props, ref) => {
+
+    const soundScript = props.soundScript;
+
+    const objectInjection = {
+        customValue: 50,
+    }
+
+    const jsInjection = `
+    // Try catch for debugging
+    try {
+        window.getRndInt = (min, max) => {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+    } catch (e) {
+        alert("Error: " + e.message);
+    }
+    true; // note: this is required, or you'll sometimes get silent failures
+    `
+
     return (
         <WebView
             style={{ maxWidth: 0, maxHeight: 0 }}
@@ -9,15 +28,34 @@ const AudioPlayer = forwardRef<WebView, { soundScript: string} >(({ soundScript 
             originWhitelist={['*']}
             ref={ref}
             //TODO: Make playback work when ringer on silent (iOS)
+            webviewDebuggingEnabled={true}
+            // produce useful error messages
+            injectedJavaScriptBeforeContentLoaded={`
+                window.onerror = function(message, sourcefile, lineno, colno, error) {
+                    alert("Message: " + message + " - Source: " + sourcefile + " Line: " + lineno + ":" + colno);
+                    return true;
+                };
+                true;
+            `}
+            onMessage={(event) => {}} // An onMessage event is required as well to inject the JavaScript code into the WebView.
+            injectedJavaScript={ jsInjection }
+            injectedJavaScriptForMainFrameOnly={false}
+            injectedJavaScriptObject={ objectInjection } // Leaving this so I remember that it's possible, may be useful
             source={{
                 html: `
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.40/Tone.js"></script>
-                <script>
-                function playSound() {
-                    ${soundScript}
-                }
-                </script>
-                `,
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.40/Tone.js"></script>
+                    <script>
+                    window.onload = (event) => {
+                        if (window.ReactNativeWebView.injectedObjectJson()) {
+                            const customValue = JSON.parse(window.ReactNativeWebView.injectedObjectJson()).customValue;
+                        }
+                    }
+
+                    function playSound() {
+                        ${soundScript}
+                    }
+                    </script>
+                    `,
             }}
         />
     );
--- constants/Exercises.ts
+++ constants/Exercises.ts
@@ -5,14 +5,22 @@ export enum ExerciseGroupings {
     Advanced = "Advanced"
 }
 
+function randomMidi() {
+    return Math.floor(Math.random() * 128);
+}
+
 export const Exercises = [
     {
-        title: "A",
+        title: "Interval Tuning",
         grouping: ExerciseGroupings.Beginner,
         soundScript: `
-        const synth = new Tone.Synth().toDestination();
-        const now = Tone.now();
-        synth.triggerAttackRelease("C4", "8n");
+        const synth0 = new Tone.Synth().toDestination();
+        const synth1 = new Tone.Synth().toDestination();
+        note0 = getRndInt(48, 79);
+        note1 = note0 + getRndInt(-12, 12);
+        synth0.triggerAttackRelease(Tone.Frequency(note0, "midi"), "2n");
+        synth1.detune.value = 20;
+        synth1.triggerAttackRelease(Tone.Frequency(note1, "midi"), "2n");
         `,
         answerChoices: [
             'In Tune',
