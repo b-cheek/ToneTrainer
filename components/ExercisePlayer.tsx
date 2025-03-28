2a85b6d0eb902684784c4cd2efb232b18b468bc5
Nic Butakow <nicholas@adastralab.com>
2025-03-28 01:19:10 -0400

    Remove ExercisePlayer text and container

--- components/ExercisePlayer.tsx
+++ components/ExercisePlayer.tsx
@@ -1,19 +1,17 @@
-import React, { useRef } from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { useRef } from 'react';
+import { View, Button } from 'react-native';
 import { WebView } from 'react-native-webview';
 import AudioPlayer from './AudioPlayer';
 
-const ExercisePlayer = (props: {soundScript: string}) => {
-
-    //TODO: Note use of tonejs offline to save sound to a buffer passed to this player instead of directly playing it?
+const ExercisePlayer = (props: { soundScript: string }) => {
+    // TODO: Note use of tonejs offline to save sound to a buffer passed to this player instead of directly playing it?
     const webview = useRef<WebView>(null);
     const playSoundInjection = `
     playSound();
     `;
 
     return (
-        <View style={styles.playerContainer}>
-            <Text>ExercisePlayer</Text>
+        <View>
             <Button title="Play" onPress={() => {
                 webview.current?.injectJavaScript(playSoundInjection);
             }} />
@@ -21,23 +19,5 @@ const ExercisePlayer = (props: {soundScript: string}) => {
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    playerContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        gap: 15,
-        marginBottom: 20,
-        backgroundColor: 'lightgray',
-        padding: 10,
-        borderWidth: 1,
-        borderColor: 'black',
-    },
-});
 
 export default ExercisePlayer;
