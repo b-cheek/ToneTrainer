30318ba681ea3be6ff44ed47e8ea85507e9669e1
b-cheek <cheekbw@gmail.com>
2025-03-29 12:56:53 -0400

    Trying to use expo asset w/ device files, need to try dev build not working now

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,9 +1,32 @@
-import React, { forwardRef } from 'react';
+import { Asset } from 'expo-asset';
+import React, { forwardRef, useEffect, useState } from 'react';
 import { WebView } from 'react-native-webview';
 
 const AudioPlayer = forwardRef<WebView, { soundScript: string} >((props, ref) => {
 
     const soundScript = props.soundScript;
+
+    const [audioFiles, setAudioFiles] = useState<{ [key: string]: string }>({});
+
+    useEffect(() => {
+        const loadAssets = async () => {
+            const sampleFiles = {
+                A2: require('../assets/trimmedSamples/bassoon/A2.mp3'),
+            };
+
+            const loadedFiles: { [key: string]: string } = {};
+            for (const [note, asset] of Object.entries(sampleFiles)) {
+                const loadedAsset = await Asset.loadAsync(asset);
+                loadedFiles[note] = loadedAsset[0].localUri || '';
+            }
+
+            setAudioFiles(loadedFiles);
+        };
+
+        loadAssets();
+    }, []);
+
+    console.log('Audio files loaded:', audioFiles);
 
     return (
         <WebView
@@ -26,6 +49,22 @@ const AudioPlayer = forwardRef<WebView, { soundScript: string} >((props, ref) =>
                 html: `
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.40/Tone.js"></script>
                     <script>
+
+                    try {
+                        const audioFiles = ${JSON.stringify(audioFiles)};
+                        alert("Audio files loaded: " + JSON.stringify(audioFiles));
+                        const A2url = audioFiles.A2;
+                        let testBuf = new Tone.Buffer(A2url, function(){alert("successful load")}, function(e){alert("failed load: " + e)});
+                        const sampler = new Tone.Sampler({urls: {A2: testBuf}}).toDestination();
+                        alert(testBuf.loaded);
+                        // const test = new Tone.Synth().toDestination();
+                        // test.triggerAttackRelease("C4", 0.5);
+                        // sampler.triggerAttackRelease("A2", 0.5);
+
+                    } catch (e) {
+                        alert("Error: " + e.message);
+                    }
+
                     function playSound() {
                         ${soundScript}
                     }
